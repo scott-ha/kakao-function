3c7d22dfe65c448a84004e181d81d345717f68de
scott-ha <haneul9209@gmail.com>
2020-12-22 18:13:29 +0900

    request

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET home page. */
+// REST API KEY
+// 6c867a9dbd34edb619b5f6eac5dacee4
+// Redirect URI
+// /oauth/getToken
+
 router.get('/', function(req, res, next) {
+  request.get({uri:"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=6c867a9dbd34edb619b5f6eac5dacee4&redirect_uri=https://hn-kakao-bmvvx.run.goorm.io/oauth/getCode"})
+});
+
+router.get('/getCode', function(req, res, next) {
+  res.render('oauth', { title: 'Express' });
+});
+
+router.get('/getToken', function(req, res, next) {
   res.render('oauth', { title: 'Express' });
 });
 
